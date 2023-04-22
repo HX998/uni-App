baf6d3ae0c6a58dca269f51984e4c41f146b1358
hx <2477468274@qq.com>
2023-04-22 22:06:26 +0800

    网易云

---
+++ pages/index/index.vue
@@ -0,0 +1,81 @@
+<template>
+	<view class="content">
+		<van-button type="primary">主要按钮</van-button>
+
+		<view class="cu-form-group">
+			<view class="title">时间选择</view>
+			<picker mode="time" :value="time" start="09:01" end="21:01" @change="TimeChange">
+				<view class="picker">
+					{{time}}
+				</view>
+			</picker>
+		</view>
+		<view class="cu-form-group">
+			<view class="title">日期选择</view>
+			<picker mode="date" :value="date" start="2015-09-01" end="2020-09-01" @change="DateChange">
+				<view class="picker">
+					{{date}}
+				</view>
+			</picker>
+		</view>
+		<view class="cu-form-group margin-top">
+			<view class="title">开关选择</view>
+			<switch @change="SwitchA" :class="switchA?'checked':''" :checked="switchA?true:false"></switch>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				time: '12:01',
+				date: '2018-12-25',
+				switchA: false,
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			TimeChange(e) {
+				this.time = e.detail.value
+			},
+			DateChange(e) {
+				this.date = e.detail.value
+			},
+			SwitchA(e) {
+				this.switchA = e.detail.value
+			},
+
+		}
+	}
+</script>
+
+<style>
+	/* .content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	} */
+</style>
---
+++ pages/login/login.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<van-button type="info" @click="login">信息按钮</van-button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			login() {
+				uni.switchTab({
+					url: '/pages/find/find'
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
