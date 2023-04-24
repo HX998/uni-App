81bd2c77bda2756cb4272be4090e13dcba236936
hx <2477468274@qq.com>
2023-04-25 00:36:03 +0800

    my.css

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -3,7 +3,7 @@
 		<view class="nav">
 			<image class="nav_img nav_more" src="../../static/more.png" mode="" />
 			<van-icon class="more_num" info="9" />
-			<image class="nav_img" src="../../static/search.png" mode="" />
+			<image class="nav_img" src="../../static/search.png" mode="" @click="searcher" />
 		</view>
 		<view class="container">
 			<view class="con_card con_hand">
@@ -19,8 +19,48 @@
 					<text class="con_info_item">Lv.7</text>
 				</view>
 			</view>
+			<view class="con_card music_app">
+				<view class="cu-list grid col-4">
+					<view class="cu-item" v-for="(item,index) in MusicApplications" :key="index">
+						<view class="lg text-red icon_image" :class="'cuIcon-' + item.image"></view>
+						<text>{{item.name}}</text>
+					</view>
+				</view>
+				<view class="card_down">
+					<text><i class="lg text-gray cuIcon-add"></i>音乐应用</text>
+				</view>
+			</view>
 			<view class="con_card"></view>
-			<view class="con_card"></view>
+		</view>
+		<scroll-view scroll-x class="bg-white nav text-center" style="height: 90upx;">
+			<view class="cu-item" v-for="(item,index) in tabs" :key="index" @click="tab_type = index"
+				:class="index===tab_type?'text-blue cur':''">
+				{{item}}
+			</view>
+		</scroll-view>
+		<view class="con_card padding" v-if="tab_type === 0">
+			<view style="display: flex;justify-content: space-between;">
+				<text>创建歌单(n个)</text>
+				<text>
+					<text class="text-gray cuIcon-add"></text>
+					<text class="text-gray cuIcon-moreandroid"></text>
+				</text>
+			</view>
+			<view class="padding" style="height: 80upx; text-align: center; color: #dedede;">
+				暂无歌单
+			</view>
+		</view>
+		<view class="con_card padding" v-if="tab_type === 1">
+			<view style="display: flex;justify-content: space-between;">
+				<text>收藏歌单(n个)</text>
+				<text>
+					<text class="text-gray cuIcon-add"></text>
+					<text class="text-gray cuIcon-moreandroid"></text>
+				</text>
+			</view>
+			<view class="padding" style="height: 80upx; text-align: center; color: #dedede;">
+				暂无歌单
+			</view>
 		</view>
 	</view>
 </template>
@@ -32,13 +72,54 @@
 	export default {
 		data() {
 			return {
-				uid: uni.getStorageSync('user').id
+				uid: uni.getStorageSync('user').id,
+				MusicApplications: [ // 音乐应用
+					{
+						image: 'videofill',
+						name: '最近播放'
+					},
+					{
+						image: 'musicfill',
+						name: '本地/下载'
+					},
+					{
+						image: 'pullup',
+						name: '云盘'
+					},
+					{
+						image: 'goodsfavor',
+						name: '已购'
+					},
+					{
+						image: 'friend',
+						name: '我的好友'
+					},
+					{
+						image: 'favorfill',
+						name: '收藏和赞'
+					},
+					{
+						image: 'wefill',
+						name: '我的播客'
+					},
+					{
+						image: 'likefill',
+						name: '乐迷团'
+					},
+				],
+				tabs: ['创建歌单', '收藏歌单'],
+				tab_type: 0
 			}
 		},
 		onLoad() {
 			this.getuserInfo()
 		},
 		methods: {
+			searcher() {
+				uni.navigateTo({
+					url: '/pages/search/search'
+				})
+			},
 			getuserInfo() {
 				let data = {
 					uid: this.uid
@@ -52,74 +133,5 @@
 </script>
 
 <style>
-	.main {
-		padding: 24upx;
-	}
-
-	.nav {
-		height: 40upx;
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.nav_img {
-		width: 36upx;
-		height: 36upx;
-	}
-
-	.nav_more {
-		position: relative;
-	}
-
-	.more_num {
-		position: absolute;
-		left: 60upx;
-	}
-
-	.container {
-		margin-top: 100upx;
-	}
-
-	.con_card {
-		background-color: #fff;
-		margin: 36upx 0;
-		border-radius: 20upx;
-		box-shadow: 0 0 30upx #d5d5d5;
-	}
-
-	.con_hand {
-		text-align: center;
-		position: relative;
-		height: 200upx;
-		margin-top: 30upx;
-	}
-
-	.sculpture {
-		position: absolute;
-		width: 120upx;
-		height: 120upx;
-		border-radius: 50%;
-		left: 50%;
-		transform: translateX(-50%);
-		top: -60upx
-	}
-
-	.con_title {
-		padding-top: 70upx;
-	}
-
-	.con_info {
-		padding: 20upx;
-	}
-
-	.con_info_item {
-		margin: 10upx;
-		color: #7c7c7c;
-	}
-
-	.septal_line {
-		position: relative;
-		top: -5upx;
-		color: #d8d8d8;
-	}
+	@import url("my.css");
 </style>
